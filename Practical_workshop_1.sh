798d945d71cdedff68d93731efde1d66942a8557
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-23 12:59:14 +0000

    New comands for keys

--- Practical_workshop_1.sh
+++ Practical_workshop_1.sh
@@ -2,3 +2,8 @@
 2: which gpg
 3: gpg --version
 4: gpg --full-generate-key
+5: gpg --list-keys
+6: gpg --armor --export estrujillo2007@gmail.com > mi_llave_publica.asc
+7: gpg --list-secret-keys --keyid-format=long
+8: gpg --armor --export-secret-keys (2E2EAF7C82661FC2)
+9: gpg --import su_llave_SY.asc (El nombre del archivo de como guarde la lleave de mi amigo)
