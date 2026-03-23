771ee0472bb33f0ebaf06ee9d9023d6af2a390dd
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-23 13:06:13 +0000

    Comprovation keys

--- Practical_workshop_1.sh
+++ Practical_workshop_1.sh
@@ -7,3 +7,4 @@
 7: gpg --list-secret-keys --keyid-format=long
 8: gpg --armor --export-secret-keys (2E2EAF7C82661FC2)
 9: gpg --import su_llave_SY.asc (El nombre del archivo de como guarde la lleave de mi amigo)
+10: echo "hi guys i am Pum ec" > doc_no_cifrado.txt
