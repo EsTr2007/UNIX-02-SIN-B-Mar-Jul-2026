2db5711fbf34b1debf118da448c8d45f46ad9ee0
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-25 13:45:25 +0000

    Test.txt

--- filesystem.sh
+++ filesystem.sh
@@ -2,4 +2,6 @@ Comandos
 pwd #Print word directory
 cd / #Change directory
 ls -F #Show the files with they tipe
-ls -i #Show the files with they identificator 
+ls -i #Show the files with they identificator
+ echo "hola" >test.txt
+ stat test.txt
