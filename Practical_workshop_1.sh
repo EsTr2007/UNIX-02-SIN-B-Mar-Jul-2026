59463262445494fc246af209dd030bd3e7c1cf43
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-25 12:31:31 +0000

    Start the interactive process to create a new pair of keys

--- Practical_workshop_1.sh
+++ Practical_workshop_1.sh
@@ -1,7 +1,7 @@
 1: uname -a #Displays detailed system information, including the kernel version and operating system architecture.
 2: which gpg #Locates the executable path of the GPG program to confirm it is installed and see where it lives.
 3: gpg --version #Checks the version number and supported algorithms of GPG.
-4: gpg --full-generate-key
+4: gpg --full-generate-key #Starts the interactive process to create a new key pair (public and private) with custom options like key type and expiration date.
 5: gpg --list-keys
 6: gpg --armor --export estrujillo2007@gmail.com > mi_llave_publica.asc
 7: gpg --list-secret-keys --keyid-format=long
