2af41e3a18d0f46a2cea8e48850da50c89686a81
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-30 13:05:41 +0000

    Combination of several commands

--- filesystem.sh
+++ filesystem.sh
@@ -8,3 +8,4 @@ ls -i #Show the files with they identificator
 sudo apt update # We use for update the list of available packages.
 sudo apt upgrade #We use for install available updates for the programs you already have.
 sudo apt install parted #We use for install the parted program on your system.
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e #It is a combination of several commands in Linux to view detailed information about disks and partitions, with separators to make it easier to read.
