6dedc5e009a366c8267e82c8abfa767b3446dbe7
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-30 13:46:38 +0000

    Create a text

--- filesystem.sh
+++ filesystem.sh
@@ -11,3 +11,4 @@ sudo apt install parted #We use for install the parted program on your system.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e #It is a combination of several commands in Linux to view detailed information about disks and partitions, with separators to make it easier to read.
 sudo parted -l #We use for display detailed information about all disks and their partitions in the system.
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #We use for find out if your system booted in UEFI or BIOS mode in Linux.
+echo "esto es un archivo" >archivo.txt #We use for create a text file and add content to it
