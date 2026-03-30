c245f88b17cfe14b68002517070a6bb0ed6c226c
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-30 12:54:30 +0000

    Update

--- filesystem.sh
+++ filesystem.sh
@@ -5,3 +5,4 @@ ls -F #Show the files with they tipe
 ls -i #Show the files with they identificator
  echo "hola" >test.txt #Create a nwe file txt
  stat test.txt #Give all tecnical dates of file
+sudo apt update # We use for update the list of available packages.
