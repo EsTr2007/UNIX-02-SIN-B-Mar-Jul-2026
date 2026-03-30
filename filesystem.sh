850fb2c59dea29254fccf7cf95a992573be5ea69
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-30 13:14:05 +0000

    System booted in UEFI o BIOS

--- filesystem.sh
+++ filesystem.sh
@@ -10,3 +10,4 @@ sudo apt upgrade #We use for install available updates for the programs you alre
 sudo apt install parted #We use for install the parted program on your system.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e #It is a combination of several commands in Linux to view detailed information about disks and partitions, with separators to make it easier to read.
 sudo parted -l #We use for display detailed information about all disks and their partitions in the system.
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #We use for find out if your system booted in UEFI or BIOS mode in Linux.
