11b1af012e38dbd5f934b67069979de5a829905d
EsTr2007 <estrujilloce@uide.edu.ec>
2026-03-30 13:00:04 +0000

    Install available updates

--- filesystem.sh
+++ filesystem.sh
@@ -6,3 +6,4 @@ ls -i #Show the files with they identificator
  echo "hola" >test.txt #Create a nwe file txt
  stat test.txt #Give all tecnical dates of file
 sudo apt update # We use for update the list of available packages.
+sudo apt upgrade #We use for install available updates for the programs you already have.
