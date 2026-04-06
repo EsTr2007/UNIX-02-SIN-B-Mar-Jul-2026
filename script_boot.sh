eec1fd343a6058f43aeb5ebe2d58335ea043b77b
EsTr2007 <estrujilloce@uide.edu.ec>
2026-04-06 12:35:33 +0000

    Change of directory

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+cd #if we write cd without arguments, the system assumes that you want to go back "home"
+cd /home/codespace #We are telling the system the exact address step by step.
+cd ~ #It is a universal alias in the terminal to represent your home directory
+cd $HOME #The system has the path of my personal folder saved in a "box" called $HOME.
