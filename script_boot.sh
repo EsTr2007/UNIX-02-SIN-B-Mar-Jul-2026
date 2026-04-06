9e7e40e773ae9ada920a0bb9e135accb68f8f12b
EsTr2007 <estrujilloce@uide.edu.ec>
2026-04-06 12:44:12 +0000

    Create a new directory

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,9 @@ cd #if we write cd without arguments, the system assumes that you want to go bac
 cd /home/codespace #We are telling the system the exact address step by step.
 cd ~ #It is a universal alias in the terminal to represent your home directory
 cd $HOME #The system has the path of my personal folder saved in a "box" called $HOME.
+mkdir proyecto #Create a new folder called project in my current location.
+cd proyecto #We went inside the folder you just created. Now my terminal is "stopped" in there.
+ls -lai # Instead of just looking at the file names, you ask the system to give you a full technical report.
+total 8
+925546 drwxr-xr-x 2 root root 4096 Apr  6 12:37 .
+714616 drwxr-xr-x 1 root root 4096 Apr  6 12:37 ..
