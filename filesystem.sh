5df28ce205cfc7081a408630ef601f3b1272f34d
EsTr2007 <estrujilloce@uide.edu.ec>
2026-04-06 12:14:49 +0000

    clean repository

--- filesystem.sh
+++
@@ -1,7 +0,0 @@
-Comandos
-pwd #Print word directory
-cd / #Change directory
-ls -F #Show the files with they tipe
-ls -i #Show the files with they identificator
- echo "hola" >test.txt #Create a nwe file txt
- stat test.txt #Give all tecnical dates of file
