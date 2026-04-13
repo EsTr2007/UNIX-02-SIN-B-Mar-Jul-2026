f1281388e5c85a354ddb799166ed369cfe06de02
EsTr2007 <estrujilloce@uide.edu.ec>
2026-04-13 12:52:52 +0000

    Create and delated new directory

--- script.sh
+++ script.sh
@@ -5,3 +5,8 @@ ls -l #Displays files in a detailed list format (permissions, owner, size, date)
 ls -h #Displays sizes in human-readable format (KB, MB, GB)
 ls -l -a -h #Combine the commands -l -a -h
 ls -lah #It's the same as the previous one, but in abbreviated form (combined flags)
+mkdir -rf #Create a new directory
+rm -rf #Deletes files and folders recursively without asking for confirmation.
+rm -- rf #Try deleting a file named rf
+mkdir -- -rf #Create a folder called -rf
+rmdir -- -rf #Deletes an empty folder named -rf
