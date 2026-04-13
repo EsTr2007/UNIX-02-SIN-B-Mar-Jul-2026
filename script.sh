c97baeee21db3c69aaaecbd0a3de044cd48d488a
EsTr2007 <estrujilloce@uide.edu.ec>
2026-04-13 12:43:55 +0000

    Variants with ls

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 Option short: ls -a #Lists all in the file
 Option long: ls --all #Lists all in the file
 #comand-options-arguments
+ls -l #Displays files in a detailed list format (permissions, owner, size, date)
+ls -h #Displays sizes in human-readable format (KB, MB, GB)
+ls -l -a -h #Combine the commands -l -a -h
+ls -lah #It's the same as the previous one, but in abbreviated form (combined flags)
