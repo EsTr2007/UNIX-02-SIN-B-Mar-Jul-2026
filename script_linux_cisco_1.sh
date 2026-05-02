d227097a50749cdd4c85f45d220f825d8c270f85
EsTr2007 <estrujilloce@uide.edu.ec>
2026-05-02 03:15:44 +0000

    ls command

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,39 @@
+ls: #Prints the path of the current directory.
+ls Documents: #
+aptitude moo: #
+ls -l: #
+ls -r: #
+ls -rl: #
+aptitude -v moo: #
+aptitude -vv moo: #
+pwd: #
+cd Documents: #
+cd /: #
+cd /home/sysadmin: #
+cd School/Art: #
+cd ..: #
+cd ~: #
+ls -1 /var/log/: #
+ls -lt /var/log: #
+ls -l -S /var/log: #
+ls -lSr /var/log: #
+ls -r /var/log: #
+su -: #
+exit: #
+sl: #
+sudo -u: #
+sudo sl: #
+cd ~/Documents: #
+ls -l hello.sh: #
+./hello.sh: #
+chmod u+x hello.sh: #
+sudo chown root hello.sh: #
+cat animals.txt: #
+cat alpha.txt: #
+head alpha.txt: #
+tail alpha.txt: #
+head -n 5 alpha.txt: #
+tail -n 5 alpha.txt: #
+cp /etc/passwd .: #
+dd if=/dev/zero of=/tmp/swapex bs=1M count=50: #
+dd if=/dev/sda of=/dev/sdb: #
