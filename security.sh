3d121e6eead9f113b19270187cd6d1964ca61213
EsTr2007 <estrujilloce@uide.edu.ec>
2026-05-06 12:39:07 +0000

    Grep command

--- security.sh
+++ security.sh
@@ -17,3 +17,16 @@ sudo groupadd -g 2000 operaciones
 #Specific GID
 #system group (GID < 1000)
 sudo groupadd --system servicios_web
+# Verify that they were created
+grep "desarrolladores\|operaciones\|servicios _web" /etc/group
+grep -E "desarrolladores|operaciones|servicios _web" /etc/group
+# View main options
+groupadd --help
+# View the range of GIDs in the system
+grep "GID _MIN\/GID_MAX\|SYS_GID" /etc/login.defs
+# System groups have a GID lower than the minimum user GID
+# In Ubuntu, typically:
+#SYS_GID_MIN = 100
+#SYS_GID_MAX = 999
+#GID_MIN = 1000
+#GID_MAX = 600000
