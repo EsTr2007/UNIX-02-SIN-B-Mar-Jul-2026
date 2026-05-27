cdabadf6992168e46ac261b862eb35b452a22690
EsTr2007 <estrujilloce@uide.edu.ec>
2026-05-27 13:39:02 +0000

    Exit the newgrp subshell

--- security2.sh
+++ security2.sh
@@ -43,3 +43,10 @@ ls -la ~/dentro_de_newgrp.txt
 # Create a directory
 mkdir -p ~/proyecto_dev/src
 ls -la ~/
+
+#project _dev/ has a 'desarrolladores' group
+#Exit the newgrp subshell
+exit
+#Verify that we returned to the original group
+id -gn
+echo "Grupo restaurado: $(id -gn)"
