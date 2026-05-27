5b3105237091d7632dae8914f285a088aa10bd3c
EsTr2007 <estrujilloce@uide.edu.ec>
2026-05-27 13:07:50 +0000

    View the current group and create file before newgrp

--- security2.sh
+++ security2.sh
@@ -5,3 +5,10 @@ id-gn #only the name of the main group
 touch~/test_inherited_group.txt
 ls -la ~/test_grupo_heredado.txt
 #The group is the user's primary group.
+
+#View the current group
+id -gn
+echo "Grupo actual: $(id -gn)"
+#Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
