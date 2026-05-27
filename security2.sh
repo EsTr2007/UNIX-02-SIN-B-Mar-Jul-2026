9ea6ed9db919b7354df183a06b3ad90945076bd5
EsTr2007 <estrujilloce@uide.edu.ec>
2026-05-27 13:02:40 +0000

    ID verification

---
+++ security2.sh
@@ -0,0 +1,7 @@
+#View current main group 
+id #Display real and effective user and groups IDs
+id-gn #only the name of the main group
+#Create a file and see which group inherits: 
+touch~/test_inherited_group.txt
+ls -la ~/test_grupo_heredado.txt
+#The group is the user's primary group.
