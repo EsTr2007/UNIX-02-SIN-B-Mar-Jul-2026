3e9e30ad2bd91163e4aa0d6743174f0a3faa982c
EsTr2007 <estrujilloce@uide.edu.ec>
2026-06-02 03:50:21 +0000

    -bash -r,-n,-x descriptions

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -11,3 +11,6 @@ echo ${OSTYPE}
 ps -e -f
 ps -ef
 df --human-readable
+#bash -r blackhatbash1.sh # -r For running restricted mode
+#bash -n blackhatbash1.sh # -n Displays syntax errors, Debugging.
+# bash -x blackhatbash1.sh # -x Enable verbose mode.
