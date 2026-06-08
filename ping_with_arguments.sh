575f2b296f1f8ff2546ed43a205eff19484ab929
EsTr2007 <estrujilloce@uide.edu.ec>
2026-06-08 13:01:16 +0000

    Verification pings

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -5,5 +5,12 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..." 
 echo "Pinging the target: ${TARGET}..." 
 ping -c 3 "${TARGET}"
+
 echo "The arguments are: $@"
 echo "The total number of arguments are: $#"
+
+bash -x ping_with_arguments.sh 127.0.0.1 127.0.0.1 127.0.0.1
+
+for args in "$@"; do
+ echo "${args}"
+done
